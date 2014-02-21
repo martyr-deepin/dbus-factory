df79df8e138c261bd213733c9ab388601dd33f66
jouyouyun <jouyouwen717@gmail.com>
2014-02-21 10:33:47 +0800

    Modify PolicyKit1

--- in.json/a/dbus-authority.h
+++
@@ -1,4 +0,0 @@
-/*This file is auto generate by dlib/dbus/proxyer. Don't edit it*/
-#include <QtDBus>
-QVariant unmarsh(const QVariant&);
-QVariant marsh(QDBusArgument target, const QVariant& arg, const QString& sig);
